a7e803db7ebf308fe7a54545b87f63c48b2f22fe
ChrisMooney04 <Chris2022058@live.ytech.edu>
2022-02-08 08:15:30 -0500

    EJS Start
    Started EJS

--- Exports/app.js
+++ Exports/app.js
@@ -1,7 +1,17 @@
 const csv = require('csvtojson')
 const fs = require('fs');
+const ejs = require('ejs');
+const express = require('express');
+const app = express()
+const port = 8000;
+app.use(express.urlencoded({
+  extended: false
+}))
+app.set('view engine', 'ejs')
+app.listen(8000)
 
 var alldata = {}
+var docpacDate = []
 
 fs.readdir(__dirname + '/data', async (err, files) => {
   if (err) console.log(err)
@@ -21,7 +31,8 @@ fs.readdir(__dirname + '/data', async (err, files) => {
       for (var y in x) {
         //console.log(alldata[x][y]);
         if (alldata[x][y]["DocPac Date"] == docpacName) {
-          console.log(alldata[x][y]);
+          //console.log(alldata[x][y]);
+          docpacDate.push(alldata[x][y]);
         }
       }
     }
@@ -38,8 +49,22 @@ fs.readdir(__dirname + '/data', async (err, files) => {
       }
     }
   }
-  // Search by docpac date (ex: Dec03)
-  docpacSearch('Sep03')
-  // Search by type of assignment (ex: Print or pg.2)
-  typeSearch("pg.2")
+  // // Search by docpac date (ex: Dec03)
+  //docpacSearch('Sep03')
+  // // Search by type of assignment (ex: Print or pg.2)
+  // typeSearch("pg.2")
+
+  // Website Code
+  app.get('/', (req, res) => {
+    res.render('index')
+  });
+
+  app.post('/', (req, res) => {
+    let date = {
+      date: req.body.date
+    }
+    docpacSearch(date.date)
+    console.log(docpacDate);
+    res.render('info', docpacDate)
+  });
 });
