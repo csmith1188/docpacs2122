cdd3cfb02d1d8c39ba2d45e480606dcb524a6bda
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2022-02-09 13:48:35 -0500

    Sql Database
    Added a sql database

--- Exports/app.js
+++ Exports/app.js
@@ -13,6 +13,12 @@ fs.readdir(__dirname + '/data', async (err, files) => {
       })
     }
   }
+
+  function delay(n) {
+    return new Promise(function(resolve) {
+      setTimeout(resolve, n * 1000);
+    });
+  }
   //console.log(alldata);
   // console.log(alldata["required_documentation"]);
 
@@ -42,17 +48,51 @@ fs.readdir(__dirname + '/data', async (err, files) => {
   // Search by type of assignment (ex: Print or pg.2)
   //typeSearch("pg.2")
 
-  function dbCreate() {
-    // insert one row into the langs table
-    db.run(`INSERT INTO goals(date) VALUES(?)`, ['C'], function(err) {
-      if (err) {
-        return console.log(err.message);
+  async function dbInsert() {
+    for (var x in alldata) {
+      for (var y in alldata[x]) {
+        await delay(0.1);
+        if (x == "goals" || x == "changes") {
+          // insert one row into the langs table
+          let db = new sqlite3.Database('db/database.db');
+          console.log(x);
+          db.run(`INSERT INTO "${x}" (date, text) VALUES("${alldata[x][y]["DocPac Date"]}", "${alldata[x][y][x]}")`, function(err) {
+            if (err) {
+              return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.changes}`);
+          });
+          // close the database connection
+          db.close();
+        } else if (x == "included" || x == "required") {
+          // insert one row into the langs table
+          let db = new sqlite3.Database('db/database.db');
+          console.log(x);
+          db.run(`INSERT INTO "${x}" (date, type, name) VALUES("${alldata[x][y]["DocPac Date"]}", "${alldata[x][y]["Type"]}", "${alldata[x][y]["Assignment Name"]}")`, function(err) {
+            if (err) {
+              return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.changes}`);
+          });
+          // close the database connection
+          db.close();
+        } else if (x == "events") {
+          // insert one row into the langs table
+          let db = new sqlite3.Database('db/database.db');
+          console.log(x);
+          db.run(`INSERT INTO "${x}" (date, event, type, text) VALUES("${alldata[x][y]["DocPac Date"]}", "${alldata[x][y]["Event Date"]}", "${alldata[x][y]["Type"]}", "${alldata[x][y]["Event Text"]}")`, function(err) {
+            if (err) {
+              return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.changes}`);
+          });
+          // close the database connection
+          db.close();
+        }
       }
-      // get the last insert id
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
-    });
-
-    // close the database connection
-    db.close();
+    }
   }
 });
