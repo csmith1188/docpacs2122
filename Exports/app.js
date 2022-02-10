902dd4af1fad053b085cfa05054c796a3d08bdc5
Jlara2005 <90395076+Jlara2005@users.noreply.github.com>
2022-02-10 10:44:50 -0500

    done the selected documents

--- Exports/app.js
+++ Exports/app.js
@@ -25,41 +25,6 @@ app.post('/', function(req,res){
   var found = 0
   var founddata = []
   if (req.body.search || req.body.dropdown != 'all'){
-<<<<<<< HEAD
-
-
-
-if (req.body.dropdown == 'goals'){
-
-console.log(db.all("SELECT * FROM Goals   ", function(err, rows) {
-  rows.forEach(function (row) {
-    console.log(row);
-    if (req.body.search){
-    if (row.date.includes(req.body.search)){
-      search.push(row)
-}
-} else {
-  search.push(row)
-}
-
-
-  })
-  console.log(search);
-  res.render('search', {error:"",  found:search, date: req.body.search });
-  search = []
-}));}
-
-
-
-if (req.body.dropdown == 'goals'){
-
-console.log(db.all("SELECT * FROM Goals   ", function(err, rows) {
-  rows.forEach(function (row) {
-    console.log(row);
-    if (row.date.includes(req.body.search)){
-      search.push(row)
-
-=======
     if (req.body.dropdown == 'goals'){
       console.log(db.all("SELECT * FROM Goals   ", function(err, rows) {
         rows.forEach(function (row) {
@@ -77,19 +42,72 @@ console.log(db.all("SELECT * FROM Goals   ", function(err, rows) {
         search = []
       }));
     }
-    if (req.body.dropdown == 'goals'){
-      console.log(db.all("SELECT * FROM Goals   ", function(err, rows) {
+    if (req.body.dropdown == 'events'){
+      console.log(db.all("SELECT * FROM Events   ", function(err, rows) {
         rows.forEach(function (row) {
           console.log(row);
             if (row.date.includes(req.body.search)){
               search.push(row)
-            }
+            } else{ search.push(row) }
         })
           console.log(search);
           res.render('search', {error:"",  found:search, date: req.body.search });
           search = []
       }));
->>>>>>> pr/8
+    }
+
+    if (req.body.dropdown == 'changes'){
+      console.log(db.all("SELECT * FROM Changes   ", function(err, rows) {
+        rows.forEach(function (row) {
+          console.log(row);
+          if (req.body.search){
+            if (row.date.includes(req.body.search)){
+              search.push(row)
+            }
+          } else {
+            search.push(row)
+          }
+        })
+        console.log(search);
+        res.render('search', {error:"",  found:search, date: req.body.search });
+        search = []
+      }));
+    }
+
+    if (req.body.dropdown == 'incdoc'){
+      console.log(db.all("SELECT * FROM IncludedDocumentation   ", function(err, rows) {
+        rows.forEach(function (row) {
+          console.log(row);
+          if (req.body.search){
+            if (row.date.includes(req.body.search)){
+              search.push(row)
+            }
+          } else {
+            search.push(row)
+          }
+        })
+        console.log(search);
+        res.render('search', {error:"",  found:search, date: req.body.search });
+        search = []
+      }));
+    }
+
+    if (req.body.dropdown == 'reqdoc'){
+      console.log(db.all("SELECT * FROM RequiredDocumentation   ", function(err, rows) {
+        rows.forEach(function (row) {
+          console.log(row);
+          if (req.body.search){
+            if (row.date.includes(req.body.search)){
+              search.push(row)
+            }
+          } else {
+            search.push(row)
+          }
+        })
+        console.log(search);
+        res.render('search', {error:"",  found:search, date: req.body.search });
+        search = []
+      }));
     }
   } else {
       res.render('search', {
