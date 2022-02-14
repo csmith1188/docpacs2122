77a6ce573c7260fd529d8902ee1e5074ebadb771
Oley Birkeland <john2022026@live.ytech.edu>
2022-02-14 11:00:11 -0500

    Don't write to database if data is missing

--- Exports/exp.js
+++ Exports/exp.js
@@ -20,19 +20,23 @@ app.post('/create',(req, res) => {
     res.send("Error: No DocPac date");
   } else {
     let categories = ["changes", "events", "goals", "included", "required"];
+    missingData = false;
     categories.forEach(category => {
       if (req.body[category + "Type"] && !req.body[category]) {
+        missingData = true;
         res.send("Error: Not enough data");
       }
     });
-    db.serialize(() => {
-      if (req.body.changes) db.run(`INSERT INTO changes ("docPacDate", "text") VALUES ("${req.body.date}", "${req.body.changes}")`);
-      if (req.body.events) db.run(`INSERT INTO events ("docPacDate", "eventDate", "type", "text") VALUES ("${req.body.date}", "${req.body.eventDate}", "${req.body.eventsType}", "${req.body.events}")`);
-      if (req.body.goals) db.run(`INSERT INTO goals ("docPacDate", "goal") VALUES ("${req.body.date}", "${req.body.goals}")`);
-      if (req.body.included) db.run(`INSERT INTO includedDocumentation ("docPacDate", "type", "assignmentName") VALUES ("${req.body.date}", "${req.body.includedType}", "${req.body.included}")`);
-      if (req.body.required) db.run(`INSERT INTO requiredDocumentation ("docPacDate", "type", "name") VALUES ("${req.body.date}", "${req.body.requiredType}", "${req.body.required}")`);
-    });
-    res.send("Inserted into database.");
+    if (!missingData) {
+      db.serialize(() => {
+        if (req.body.changes) db.run(`INSERT INTO changes ("docPacDate", "text") VALUES ("${req.body.date}", "${req.body.changes}")`);
+        if (req.body.events) db.run(`INSERT INTO events ("docPacDate", "eventDate", "type", "text") VALUES ("${req.body.date}", "${req.body.eventDate}", "${req.body.eventsType}", "${req.body.events}")`);
+        if (req.body.goals) db.run(`INSERT INTO goals ("docPacDate", "goal") VALUES ("${req.body.date}", "${req.body.goals}")`);
+        if (req.body.included) db.run(`INSERT INTO includedDocumentation ("docPacDate", "type", "assignmentName") VALUES ("${req.body.date}", "${req.body.includedType}", "${req.body.included}")`);
+        if (req.body.required) db.run(`INSERT INTO requiredDocumentation ("docPacDate", "type", "name") VALUES ("${req.body.date}", "${req.body.requiredType}", "${req.body.required}")`);
+      });
+      res.send("Inserted into database.");
+    }
   }
 })
 app.post('/search', (req, res) => {
