8d28573da30bf92c0fa92889e18f5a1f0db2669b
Riley23062 <82524320+Riley23062@users.noreply.github.com>
2022-02-22 10:50:44 -0500

    fixes
    sss

--- application/app.js
+++ application/app.js
@@ -14,6 +14,18 @@ app.get("/neworder", (req, res) => {
   res.render("neworder.ejs");
 })
 
+
+app.get('/additem', (req, res) => {
+  res.render('additem')
+})
+
+
+app.post('/additem', (req, res) =>{
+res.send('dATA HAS BEEN GOTEN')
+
+})
+
+
 app.listen(port, () => {
   console.log(`Port ${port} connected. Welcome to the Worse Company Delivery Management System.`)
 })
