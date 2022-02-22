54bfc9068cc56c1590405111ea53b71c18ee01e7
Oley Birkeland <john2022026@live.ytech.edu>
2022-02-22 10:37:00 -0500

    Move files to correct location

--- nocti_prep/g_preppack/application/app.js
+++ nocti_prep/g_preppack/application/app.js
@@ -1,3 +1,23 @@
+const ejs = require('ejs');
+const express = require('express');
+const app = express()
+const port = 9000
+const fs = require('fs');
+
+var rawdata = fs.readFileSync('data.json');
+var data = JSON.parse(rawdata);
+
+app.use(express.urlencoded({  extended: true  }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("homepage.ejs");
+})
+
+app.get("/neworder", (req, res) => {
+  res.render("neworder.ejs");
+})
+
 //The res.send statements need to be replaced with an EJS template containing a message and a link to "/"
 app.post("/neworder", (req, res) => {
   let formdata = req.body;
@@ -13,6 +33,11 @@ app.post("/neworder", (req, res) => {
       total: 0
     });
     res.send(`Order ${data.orders.length} was created`);
-    fs.writeFile("data.json", JSON.stringify(data), () => console.log("Wrote to file"));
+    data = JSON.stringify(data);
+    fs.writeFile("data.json", data, () => console.log("Wrote to file"));
   }
 });
+
+app.listen(port, () => {
+  console.log(`Port ${port} connected. Welcome to the Worse Company Delivery Management System.`)
+})
