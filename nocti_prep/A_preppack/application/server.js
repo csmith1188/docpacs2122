8ad59a57226b8d4a8ba16ad4d16836b84f1988c3
Leniece <lenie22005@live.ytech.edu>
2022-02-24 08:28:37 -0500

    Port and console.log

--- nocti_prep/A_preppack/application/server.js
+++ nocti_prep/A_preppack/application/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const ejs = require('ejs');
 var app = express();
+const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: false}));
 //change view engine
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+
+})
+
+console.log("Server is running on localhost:8080");
