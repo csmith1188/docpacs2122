01a9043e2fbbb8e6f4dfb90e47f7214772a10567
Aidan4895 <90394687+Aidan4895@users.noreply.github.com>
2022-02-24 11:11:08 -0500

    Work for Feb 24th

--- nocti_prep/application/application.js
+++ nocti_prep/application/application.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-const Math = require('Math');
 var app = express()
 app.set("view engine", 'ejs')
 app.use(express.urlencoded({extended: true}))
@@ -59,6 +58,8 @@ app.post('/neworder', function(req, res){
 })
 
 app.post('/additem', function(req, res){
+  var rawdata = fs.readFileSync('data.json', function(err,data){})
+  var data = JSON.parse(rawdata)
   if (req.body.num == false || req.body.itemname == false || req.body.qty == false || req.body.price == false) {
     res.render('additem', {
       error: "You Forgot Something"
@@ -67,8 +68,19 @@ app.post('/additem', function(req, res){
     res.render('additem', {
       error: "THE ORDER IS IN THE BLACK ABYESS WHERE NOTHING EXIST please put in order number of this realm"
     })
+    console.log(data.data.length);
   } else {
     data.data[req.body.num - 1].items.push({itemname: req.body.itemname, quantity: req.body.qty, price: req.body.price })
+    data.data[req.body.num - 1].subtotal = 0
+    data.data[req.body.num - 1].tax = 0
+    data.data[req.body.num - 1].total = 0
+    data.data[req.body.num - 1].items.forEach((item, i) => {
+      var opsub = this.price * this.quantity
+      data.data[req.body.num - 1].subtotal = opsub + data.data[req.body.num - 1].subtotal
+      this.tax = this.subtotal * 0.06
+      this.total = this.tax + this.subtotal
+    });
+
     var stringdata = JSON.stringify(data)
     fs.writeFileSync("data.json",stringdata, "utf8")
     res.render('additem', {
