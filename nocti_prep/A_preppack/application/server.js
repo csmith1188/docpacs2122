f9dc8ea9f0fb6ed2f68a11c12e6e9c5289462db7
parke2023059 <90394656+parke2023059@users.noreply.github.com>
2022-02-24 10:15:08 -0500

    3

--- nocti_prep/A_preppack/application/server.js
+++ nocti_prep/A_preppack/application/server.js
@@ -12,25 +12,47 @@ app.get('/', function(req,res){
   res.render('index.ejs')
 })
 
+
+
 app.get('/neworder', function(req,res){
-  res.render('neworder.ejs')
+  res.render('neworder.ejs', {
+    error:""
+  })
 })
+
+
 
 app.post('/neworder', function(req,res){
   const name = req.body.cName
   const address = req.body.cAddress
-})
+  const user = {
+    usrName: name,
+    usrAddress: address
+  }
+  if(name == ""){
+    res.render('neworder.ejs', {
+      error: "Must fill out Name field"
+    })
+  }
 
-
+  if(address == ""){
+    res.render('neworder.ejs', {
+      error: "Must fill out Address field"
+  })
+}
 
 
 app.get('/additem', function(req,res){
   res.render('additem.ejs')
 })
 
+
+
 app.get('/view', function(req,res){
   res.render('view.ejs')
 })
+
+
 
 app.listen(port, ip, function(){
   console.log("Server Up: ");
