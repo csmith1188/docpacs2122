cf3782372acdcb732aaba139904772205a32f78d
parke2023059 <90394656+parke2023059@users.noreply.github.com>
2022-02-25 14:10:59 -0500

    5

--- nocti_prep/A_preppack/application/server.js
+++ nocti_prep/A_preppack/application/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const fs = require('fs');
 const port = 8080
 const ip = '127.0.0.1'
 app.set('view engine', 'ejs')
@@ -16,7 +17,8 @@ app.get('/', function(req,res){
 
 app.get('/neworder', function(req,res){
   res.render('neworder.ejs', {
-    error:""
+    error:"",
+    alert: ""
   })
 })
 
@@ -25,15 +27,33 @@ app.get('/neworder', function(req,res){
 app.post('/neworder', function(req,res){
   const name = req.body.cName
   const address = req.body.cAddress
+  const number = req.body.cOrder
   const user = {
     usrName: name,
-    usrAddress: address
+    usrAddress: address,
+    usrOrder: number
     }
     if(name == "" || address == ""){
       res.render('neworder.ejs', {
         error: "Must Fill Out All Fields"
       })
     }
+
+    const order = {
+      orderNumber: number ,
+      order:1 ,
+      customerName: name ,
+      customerAddress: address,
+      item: [],
+      subtotal: 0,
+      tax: 0,
+      total: 0
+    }
+    console.log(order);
+    outData = JSON.stringify(order)
+    fs.writeFileSync('data.json', outData, function(){
+      console.log("Data Saved: ");
+    })
   }
 )
 
