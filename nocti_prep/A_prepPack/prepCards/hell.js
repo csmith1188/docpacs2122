c0d7013531d2d55896c8cfb76a6362a4b9283e6b
Skayote <90394867+TFloto@users.noreply.github.com>
2022-03-13 11:38:00 -0400

    3/13

---
+++ nocti_prep/A_prepPack/prepCards/hell.js
@@ -0,0 +1,50 @@
+//{author: "Trey Floto", question: answers: type: "multi" key: 2}
+general information and concepts
+{author: "Trey Floto", question: "What is the following errors is caused by  calculation error?", answers: ["logical", "Arithmatic", "truncation", "semantic"], type: "multi", key: 2}
+{author: "Trey Floto", question: "Which of the following uses the three basic control structures to form highly structured units of code?", answers: ["structured programming", "circular connector", "modular construction", "loop structure"], type: "multi", key: 1}
+{author: "Trey Floto", question: "what type of principle is used for configuring preferences that allow the user to personalise an application", answers: ["CLI", "GUI", "API", "DOS"], type: "multi", key:1}
+{author: "Trey Floto", question: "All computer instructions are based on four basic logic patterns. these patterns are simple sequence, selection, loop, and...", answers: ["trailer", "modular", "top-down", "branch"], type: "multi" key: 2}
+{author: "Trey Floto", question: "if the memory speed of the bus is 750MHz, and the CPU is operating at twice the speec of the bus, what is the speed of the processor?", answers: ["375MHz", "750MHz", "1.5GHz", "2GHz"], type: "multi" key: 3}
+{author: "Trey Floto", question: "two types of programs that create machine code are the interpreter and the...", answers: ["assembler", "object", "compiler", "source"], type: "multi" key: 1}
+{author: "Trey Floto", question: "in the order of calculations in a program", answers: ["addition precedes division", "parenthesis precedes exponentation", "subtraction precedes addition", "multiplication precedes exponentation"], type: "multi" key: 2}
+
+ethics, legal compliance, and security
+
+{author: "Trey Floto", question: "when the code below is excecuted, strAdd will be equal to",  answers: ["579", "123 + 456", "123456", "strFirst + strSecond"], type: "multi", key: 3}
+{author: "Trey Floto", question: "if a programmer downloads a copy of Microsoft SQL server from a file-sharing server website without paying for it, the programmer has commited an offense called...", answers: ["piracy", "larceny", "felony", "spyware"],type: "multi", key: 1}
+{author: "Trey Floto", question: "The Sarbanes-Oxley Act requires monitoring and documentation of transactions. How could a programmer mistakenly set up a program that would cause problems with compliance with SOX", answers: ["create system logs of the transactions taken in the session", "require a user to click three times when they are sure they want to precede", "help a user to request assistance from the system administrators", "utilize system credentials instead of pass-through credentials"], type: "multi", key: 4}
+{author: "Trey Floto", question: "The following programming flowchart symbol is a/an...", answers: ["processing", "input/output", "decision"],type: "multi", key:3}
+{author: "Trey Floto", question: "one of the main goals in analyzing user requirements is to...", answers: ["troubleshoot the problem,", "shutdown and maintain the system", "keep the customer happy", "elicit requirements from a users point of view"],type: "multi", key:3}
+{author: "Trey Floto", question: "if a record has the following field lengths, what is the record length", answers: ["42", "47", "54", "56"],type: "multi", key:2}
+{author: "Trey Floto", question: "The following programming flowchart symbol is a/an...", answers: ["processing", "input/output", "decision", "test"], type: "multi", key: 2}
+{author: "Trey Floto", question: "a programmer is starting a new programming project. during what phase of the programming project should the programmer begin with planning for data storage", answers: ["billing", "documentation", "implementation", "test"],type: "multi", key:3}
+{author: "Trey Floto", question: "a progammer needs to manipulate data that consists of a sequence of numbers that must be used as both an individual object and a set of data. what is the best data structure the programmer can use", answers: ["array", "constant", "string", "field"], type: "multi", key:1}
+{author: "Trey Floto", question: "when a programmer is ready to create an excecutable code, what process is used", answers: ["debugging", "editing", "compiling", "dissasembling"], type: "multi", key:3}
+
+design program solutions
+{author: "Trey Floto", question: "the source document and _____ should follow the same order", answers: ["input screen design", "output screen design", "decision- type operator symbols", "processing function symbols"], type: "multi", key: 1}
+{author: "Trey Floto", question: "which of the following is a correct name for a variable", answers: ["State_Zipcode", "State+Zipcode", "State Zipcode", "State=Zipcode"], type: "multi", key: 1}
+{author: "Trey Floto", question: "which of the following expression is invalid? A=10 + New Data", answers: ["variable 5 is not defined", "data types are mixed", "New Data is not a string", "New Data = 0"], type: "multi", key: 2}
+{author: "Trey Floto", question: "an output field has Los Angeles moved to it and when examined contains Los Angeles. This indicates a/an _____ error", answers: ["logical", "arithmatic", "truncation", "semantic"], type: "multi", key: 3}
+{author: "Trey Floto", question: "A programmer needs to define the type of value for a variable. what pre-defined set of characteristics for a variable can the programmer choose", ["storage location", "return value", "method modifier", "data type"], type: "multi", key: 2}
+
+code programs
+{author: "Trey Floto", question: "one example of scripting language is...", answers: ["Perl", "C++", "Fortran", "Beta"], type: "multi", key: 2}
+{author: "Trey Floto", question: "which of the following use plain english queries", answers: ["BASIC", "FORTRAN", "SQL", "C++"], type: "multi", key: 4}
+{author: "Trey Floto", question: "an example of a loop structure is? ", answers: ["DO UNTIL", "SLELECT IF", "EXCECUTE", "NEXT"], type: "multi", key: 1}
+{author: "Trey Floto", question: "compile errors can be caused by ", answers: ["bad syntax", "input data", "insufficient test data", "incorrect user specification"], type: "multi", key: 1}
+
+test programs
+{author: "Trey Floto", question: "Typically after successful integration testing, a ______ test is performed to compile a fromt to back ececution of the entire application", answers: ["system", "unit", "fuzz", "beta"], type: "multi", key: 2}
+{author: "Trey Floto", question: "the implementation phase consists of coding, testing, and...", answers: ["designing", "building prototype programs", "establish requirements", "integration"], type: "multi", key: 3}
+{author: "Trey Floto", question: "when TEXT1.VALUE= March 7, TEXT1_LEN= ", answers: ["7", "8", "9", "10"], type: "multi", key: 4}
+{author: "Trey Floto", question: "A data element is intended to contain a range from 15 to 30 and after testing it contains 32. what kind of error is this", answers: ["truncation", "rounding", "run time", "boundary"], type: "multi", key: 4}
+{author: "Trey Floto", question: "which of the following consists of a sequence of events to follow so they can be repeated.", answers: ["runtime", "test files", "trace route", "script"], type: "multi", key: 1}
+
+maintain programs and impliment change manegment procedures
+{author: "Trey Floto", question: "maintinence refers to changes to a system in operating arising from error corrections, performance improvements, changes in business enviroment and...", answers: ["enhancements in function", "application requirements", "referencing software", "material requirements"], type: "multi", key: 1}
+{author: "Trey Floto", question: "one method of ensuring that updates to documentation are efficiently distributed is to", answers: ["send customers printed documentation annually reguardless of whether there have been changes", "make changes in an online help system rather than printed sources", "send fliers containing only the changes rather than new printed volumes", "make changes only on a pre-specific schedule"], type: "multi", key: 1}
+
+complete user documentation and technical writing
+{author: "Trey Floto", question: "a programmer has to make programming changes to an existing program. therefore the comments in the existing program must also be updated. the comments are refered to as _____ documentation. ", answers: ["external", "internal", "primary", "secondary"], type: "multi", key: 4}
+{author: "Trey Floto", question: "a programmer has decided to create a pop up window that will display a brief description of a control purpose when the mouse hovers over it. this popup is known as a...", answers: ["hyper clip", "too tip", "task bar", "help bar"], type: "multi", key: 2}
